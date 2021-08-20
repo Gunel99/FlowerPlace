482345313a9701527f92e156e616b50acdc9fbf2
Gunel99 <45911420+Gunel99@users.noreply.github.com>
2021-08-20 19:24:09 +0400

    Completed task

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,17 @@ for (let menu of menus) {
 }
 
 $(document).ready(function () {
+  $("img.bg-image").each((index, item) => {
+    let div = $('<div/>', {
+      class: 'bg-image-viewer'
+    });
+
+    let src = $(item).attr('src');
+    $(div).css('background-image', `url(${src})`);
+    $(div).insertAfter(item);
+    $(div).append(item);
+  });
+
   // Carousel in tab menu start
   $("#tab-menu .prev-next-icons button.slick-n").click(function () {
     $("#tab-menu .comments").slick("slickNext");
@@ -127,5 +138,78 @@ $(document).ready(function () {
         },
       },
     ],
+  });
+
+  // Product images carousel start
+  $("#product-detail .product .prev-next-icons button.slick-n").click(function () {
+    $("#product-detail .product-images").slick("slickNext");
+  });
+  $("#product-detail .product .prev-next-icons button.slick-p").click(function () {
+    $("#product-detail .product-images").slick("slickPrev");
+  });
+  
+  $("#product-detail .product-images.multiple-items").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+
+  $("#product-detail .product-detail .prev-next-icons button.slick-n").click(function () {
+    $("#product-detail .detailed-images").slick("slickNext");
+  });
+  $("#product-detail .product-detail .prev-next-icons button.slick-p").click(function () {
+    $("#product-detail .detailed-images").slick("slickPrev");
+  });
+
+  $("#product-detail .product-detail .detailed-images.multiple-items").slick({
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+   
+  });
+
+  // Product detail start
+  $("#product-detail .product-detail .about .choose-postcard").click(function (e) {
+    e.preventDefault();
+
+    $("#product-detail .product-detail .detailed-images.multiple-items").slick('unslick').slick({
+      slidesToShow: 5,
+      slidesToScroll: 2,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+
+    $("#product-detail .product-detail .product-detail-border").fadeToggle(500);
+  });
+
+  $("#add-comment").change(function () {
+    $("#add-comment").closest('.add-comments').next().fadeToggle(500);
   });
 });
